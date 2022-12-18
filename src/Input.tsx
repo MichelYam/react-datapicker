7a58c9f357ccda4df4bc1d81eef5dc570ff7bf79
MichelYAM <michel.yam92@gmail.com>
2022-12-18 23:43:25 +0100

    init babel

---
+++ src/Input.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function Input() {
+  const [date, setDate] = useState(new Date())
+  const [focus, setFocus] = useState(false)
+  const onFocus = () => {
+    setFocus(!focus)
+  }
+  console.log(focus)
+  return (
+    <div className="App">
+      <input type="text" onFocus={onFocus}  onBlur={onFocus} />
+      <div className={`${focus ? "up" : "no"}`}>
+        <p>Hi im am a calendar</p>
+      </div>
+    </div>
+  );
+}
+
+export default Input;
