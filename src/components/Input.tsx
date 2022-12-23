9a05d0738f8b716c0e62f51a731fdbd27a845706
MichelYAM <michel.yam92@gmail.com>
2022-12-23 13:40:17 +0100

    test

---
+++ src/components/Input.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+import './App.css';
+
+
+export function Input() {
+  const [date, setDate] = useState(new Date())
+  const [focus, setFocus] = useState(false)
+
+  const onFocus = () => {
+    setFocus(!focus)
+  }
+  return (
+    <div>
+      < input type="text" onFocus={onFocus} onBlur={onFocus} />
+      <div className={`${focus ? "up" : "no"}`}>
+        <p>message displayed</p>
+      </div>
+    </div>
+  );
+}
+
